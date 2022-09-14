999009379d217f10f3241b8cd79b5f1a1ec06add
abel <abelalx3d@gmail.com>
2022-09-14 02:35:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,87 @@
+Task 0.txt
+
+Who has access
+
+A
+
+System properties
+
+Type
+
+Text
+
+Size
+
+271 bytes
+
+Storage used
+
+271 bytes
+
+Location
+
+0x02. C - Functions, nested loops
+
+Owner
+
+Antony Bahati
+
+Modified
+
+Sep 13, 2022 by Antony Bahati
+
+Opened
+
+12:33 PM by me
+
+Created
+
+Sep 13, 2022
+
+No description
+
+Viewers can download
+
+#include "main.h"
+
+
+
+/**
+
+ * main - print putchar
+
+ * Return: always 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+
+
+	return (0);
+
+
+
+}
